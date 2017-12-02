b1766fdc6e7a3a810defbfa0904625133e653350
Jane Chen <jane.chen@macaulay.cuny.edu>
2017-12-02 13:30:24 -0500

    added final touches to styles

--- script.js
+++ script.js
@@ -16,13 +16,12 @@ function showWeather(position) {
     var long = position.coords.longitude;
 
     var url = "https://fcc-weather-api.glitch.me/api/current?lat=" + lat + "&lon=" + long;
-    // console.log(url);
 
     $.getJSON(url, function(data) {
         console.log(data.main.temp);
 
         var temp = document.getElementById("temp");
-        temp.innerHTML = data.main.temp + 'C';
+        temp.innerHTML = data.main.temp.toFixed(2) + '&degC';
 
         var toggler = document.getElementById("toggler");
         toggler.innerHTML += '<button class="button" onclick="toggleUnits()">Toggle F/C</button><br>';
@@ -32,6 +31,7 @@ function showWeather(position) {
 
         var weather = document.getElementById("weather");
         weather.innerHTML = data.weather[0].main;
+        setBackground(data.weather[0].main);
     });
 }
 
@@ -41,10 +41,10 @@ function toggleUnits() {
     var degrees = parseFloat(tempContents);
 
     if (tempContents[tempContents.length - 1] == 'C') {
-        temp.innerHTML = (degrees * 9 / 5 + 32).toFixed(2) + 'F';
+        temp.innerHTML = (degrees * 9 / 5 + 32).toFixed(2) + '&degF';
     }
     else {
-        temp.innerHTML = ((degrees - 32) * 5 / 9).toFixed(2) + 'C';
+        temp.innerHTML = ((degrees - 32) * 5 / 9).toFixed(2) + '&degC';
     }
 }
 
@@ -63,6 +63,34 @@ function showError(error) {
             break;
         case error.UNKNOWN_ERROR:
             x.innerHTML = "An unknown error occurred.";
+            break;
+    }
+}
+
+function setBackground(weather) {
+    var html = document.getElementsByTagName('html')[0];
+    
+    switch (weather) {
+        case 'Drizzle':
+            html.style.backgroundImage = 'url(img/drizzle.jpg)';
+            break;
+        case 'Clear':
+            html.style.backgroundImage = 'url(img/clear.jpg)';
+            break;
+        case 'Clouds':
+            html.style.backgroundImage = 'url(img/clouds.jpg)';
+            break;
+        case 'Mist':
+            html.style.backgroundImage = 'url(img/mist.jpg)';
+            break;
+        case 'Rain':
+            html.style.backgroundImage = 'url(img/rain.jpg)';
+            break;
+        case 'Snow':
+            html.style.backgroundImage = 'url(img/snow.jpg)';
+            break;
+        case 'Thunderstorm':
+            html.style.backgroundImage = 'url(img/thunderstorm.jpg)';
             break;
     }
 }
